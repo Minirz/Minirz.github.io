18fc4cbcf0f5999f386d0ceaef6dcd046fc6830e
Victor Clausen <100559970+Minirz@users.noreply.github.com>
2023-03-07 12:20:20 +0100

    custom mouse cursor

--- src/js/index.js
+++ src/js/index.js
@@ -100,3 +100,54 @@ window.onload = () => {
     }, 800);
   }
 };
+
+//* =========== MOUSE CURSOR SCRIPT START =========== *\
+const coords = { x: 0, y: 0 };
+const circles = document.querySelectorAll(".circle");
+
+const colors = [
+  "#02a5e0",
+  "#038fce",
+  "#057bbc",
+  "#0669ab",
+  "#06589a",
+  "#074989",
+  "#073c78",
+  "#073068",
+];
+
+circles.forEach(function (circle, index) {
+  circle.x = 0;
+  circle.y = 0;
+  circle.style.backgroundColor = colors[index % colors.length];
+});
+
+window.addEventListener("mousemove", function (e) {
+  coords.x = e.clientX;
+  coords.y = e.clientY;
+});
+
+function animateCircles() {
+  let x = coords.x;
+  let y = coords.y;
+
+  circles.forEach(function (circle, index) {
+    circle.style.left = x - 12 + "px";
+    circle.style.top = y - 12 + "px";
+
+    circle.style.scale = (circles.length - index) / circles.length;
+
+    circle.x = x;
+    circle.y = y;
+
+    const nextCircle = circles[index + 1] || circles[0];
+    x += (nextCircle.x - x) * 0.3;
+    y += (nextCircle.y - y) * 0.3;
+  });
+
+  requestAnimationFrame(animateCircles);
+}
+
+animateCircles();
+
+//! ============ MOUSE CURSOR SCRIPT END ============ !\
